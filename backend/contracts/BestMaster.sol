5950ef384f44e7a046d19812daa96ff4d507866f
Ragenchirm <noel.gagnaire@gmail.com>
2024-07-08 01:56:21 +0200

    can change the withdrawal address

--- backend/contracts/BestMaster.sol
+++ backend/contracts/BestMaster.sol
@@ -9,18 +9,18 @@ import "hardhat/console.sol";
 
 
 contract BestMaster is AccessControl {
-    address superAdminAddress; // address to withdraw contract funds
+    address public withdrawalAddress; // address to withdraw contract funds
     address[] public bestProjectsAddresses;
-    bytes32 public constant SUPER_ADMIN_ROLE = keccak256("SUPER_ADMIN_ROLE");
-    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
-    bytes32 public constant BLACKLIST_ROLE = keccak256("BLACKLIST_ROLE");
+    bytes32 constant SUPER_ADMIN_ROLE = keccak256("SUPER_ADMIN_ROLE");
+    bytes32 constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+    bytes32 constant BLACKLIST_ROLE = keccak256("BLACKLIST_ROLE");
     uint public projectPrice; 
     uint public bestFee;
     address public usdtContractAddress;
     ERC20 public tetherToken;
 
     constructor() { // ARGS are : (address _usdtContractAddress, uint _projectPrice, uint _bestFee)
-        superAdminAddress=msg.sender;
+        withdrawalAddress=msg.sender;
         _grantRole(SUPER_ADMIN_ROLE, msg.sender);
         // JUST TO TEST IN REMIX, REMOVE AFTER
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -51,12 +51,20 @@ contract BestMaster is AccessControl {
 
     function withdraw(uint _amount)external onlyRole(SUPER_ADMIN_ROLE){
         require( _amount <= tetherToken.balanceOf(address(this)),"Not enougth funds");
-        (bool success, bytes memory data) = usdtContractAddress.call(abi.encodeWithSignature("transfer(address,uint256)", superAdminAddress,_amount));
+        (bool success, bytes memory data) = usdtContractAddress.call(abi.encodeWithSignature("transfer(address,uint256)", withdrawalAddress,_amount));
     }
 
     function changeFee(uint _fee)external onlyRole(DEFAULT_ADMIN_ROLE){
         require( _fee <= 99,"Can't set more than a 99% fee");
         bestFee=_fee;
+    }
+
+    function changeWithdrawalAddress(address _newWithdrawalAddress)external onlyRole(SUPER_ADMIN_ROLE){
+        //this require is a security to not accidentaly change thewithdrawal address
+        //the process must be grant SUPER_ADMIN_ROLE to _newWithdrawalAddress
+        //then connect as _newWithdrawalAddress and changeWithdrawalAddress and if necessary renounce SUPER_ADMIN_ROLE
+        require(_newWithdrawalAddress==msg.sender,"you are not the new address");
+        withdrawalAddress=_newWithdrawalAddress;
     }
 
 
