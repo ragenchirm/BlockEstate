0265e902ab27704988c2a2ece1905c62190470e6
Ragenchirm <noel.gagnaire@gmail.com>
2024-07-11 22:46:46 +0200

    added dotenv vault

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,11 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
-const SEPOLIA_HOLESKY_PRIVATE_KEY = process.env.SEPOLIA_HOLESKY_PRIVATE_KEY;
-const HOLESKY_RPC_URL = process.env.AL_HOLESKY_RPC_URL;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-const MAINNET_RPC_URL = process.env.IN_MAINNET_RPC_URL;
-const SEPOLIA_RPC_URL = process.env.AL_SEPOLIA_RPC_URL;
+const SEPOLIA_HOLESKY_PRIVATE_KEY = process.env.SEPOLIA_HOLESKY_PRIVATE_KEY || "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";//hh user0 PK
+const HOLESKY_RPC_URL = process.env.AL_HOLESKY_RPC_URL || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const MAINNET_RPC_URL = process.env.IN_MAINNET_RPC_URL || "";
+const SEPOLIA_RPC_URL = process.env.AL_SEPOLIA_RPC_URL ||"";
 
 module.exports = {
   solidity: "0.4.17",
