9733c6baddf9c421d9421466be8a7b0dbc4365db
Ragenchirm <noel.gagnaire@gmail.com>
2024-07-14 12:37:38 +0200

    ci test

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
-const SEPOLIA_HOLESKY_PRIVATE_KEY = process.env.SEPOLIA_HOLESKY_PRIVATE_KEY || "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";//PK Hardhat 
+const SEPOLIA_HOLESKY_PRIVATE_KEY = process.env.SEPOLIA_HOLESKY_PRIVATE_KEY || "";;
 const HOLESKY_RPC_URL = process.env.AL_HOLESKY_RPC_URL || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 const MAINNET_RPC_URL = process.env.IN_MAINNET_RPC_URL || "";
